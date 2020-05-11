5c372f89c6350d9d5f2cfab7264b4647f4c4a4f2
sari.hatanaka <s-hatanaka@i-enter.co.jp>
2020-05-11 10:46:01 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 畑中 彩里 on 2020/05/11.
+//  Copyright © 2020 sari.hatanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
